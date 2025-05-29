1fe604c60eb20c68ab3e2a384198357dd56462ee
Clement Jambon <clement.jambon@gmail.com>
2025-05-29 17:41:33 -0400

    feat: add basic diffusion_save file

--- diffusion_save.py
+++ diffusion_save.py
@@ -4,6 +4,7 @@ from typing import List
 import numpy as np
 import tyro
 from tqdm.auto import tqdm
+from pathlib import Path
 
 import viser
 import viser.extras
@@ -17,6 +18,7 @@ def main(
     data_path: str = "data/interp_0001.traj",
     share: bool = False,
     i_traj: int = 0,
+    framerate: int = 30,
 ) -> None:
     server = viser.ViserServer()
     if share:
@@ -35,7 +37,7 @@ def main(
             min=0.001,
             max=0.02,
             step=1e-3,
-            initial_value=0.01,
+            initial_value=0.02,
         )
         gui_timestep = server.gui.add_slider(
             "Timestep",
@@ -93,10 +95,16 @@ def main(
     def _(_) -> None:
         gui_framerate.value = int(gui_framerate_options.value)
 
+    # Create serializer.
+    serializer = server.get_scene_serializer()
+
     point_nodes: List[List[viser.PointCloudHandle]] = []
     for t, uvgrid in tqdm(trajectory.traj_uv_grids.items()):
         t_point_nodes = []
         for i_batch in range(uvgrid.coord.shape[0]):
+
+            if i_batch > 0:
+                continue
 
             # Place the point cloud in the frame.
             uvgrid_coord = uvgrid.coord[i_batch][~uvgrid.empty_mask[i_batch]]
@@ -111,35 +119,23 @@ def main(
             points = valid_uvgrid_coord.reshape(-1, 3)
             t_point_nodes.append(
                 server.scene.add_point_cloud(
-                    name=f"/traj_{i_traj}/{i_batch}/{t}/point_cloud",
+                    # name=f"/traj_{i_traj}/{i_batch}/{t}/point_cloud",
+                    name="/point_cloud",
                     points=points,
                     colors=valid_uvgrid_colors,
                     point_size=gui_point_size.value,
                     point_shape="circle",
                 )
             )
+
+            # Add a frame delay.
+            serializer.insert_sleep(1.0 / framerate)
+
         point_nodes.append(t_point_nodes)
 
-    # Hide all but the current pc.
-    for i_t, t_point_nodes in enumerate(point_nodes):
-        for i_batch, point_node in enumerate(t_point_nodes):
-            point_node.visible = (i_t == gui_timestep.value) and (
-                i_batch == gui_batch.value
-            )
-
-    # Playback update loop.
-    prev_timestep = gui_timestep.value
-    prev_batch = gui_batch.value
-    while True:
-
-        # Update point size of both this timestep and the next one! There's
-        # redundancy here, but this will be optimized out internally by viser.
-        #
-        # We update the point size for the next timestep so that it will be
-        # immediately available when we toggle the visibility.
-        point_nodes[gui_timestep.value][
-            gui_batch.value
-        ].point_size = gui_point_size.value
+    # Save the complete animation.
+    data = serializer.serialize()  # Returns bytes
+    Path("recordings/recording.viser").write_bytes(data)
 
 
 if __name__ == "__main__":
